7c9947189e23afbc03c6fa8b0aa1ee06c2f20e12
Ybidnny <bruninha.potter.jacobsen@gmail.com>
2025-02-25 19:29:43 -0300

    Bruna teste

--- Alunos/bruna.cs
+++ Alunos/bruna.cs
@@ -1,9 +1,1 @@
 Console.WriteLine("Bruna");
-<html>
-<body>
-
-<h1>Olá, seja bem-vindo!</h1>
-<p>Me chamo Bruna, tenho 16 anos, estou cursando programação, onde faço programas, e aprendo a utilizar o chat GPT a meu favor.</p>
-
-</body>
-</html>
